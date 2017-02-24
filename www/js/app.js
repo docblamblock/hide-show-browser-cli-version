844bb2f9d3c4747a5b6ee7209b21cfe0e55857f5
docblamblock <darrenconnors@hotmail.com>
2017-02-24 15:17:17 +0000

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -1,94 +1,31 @@
-
-
-
+function geoFindMe() {
   
+  alert("test");
+  
+  var output = document.getElementById("out");
 
-var Latitude = 53;
-var Longitude = 8;
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
 
-// Get geo coordinates
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
-    
+    output.appendChild(img);
+  }
 
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
 
+  output.innerHTML = "<p>Locating…</p>";
 
-function getMapLocation() {
-    alert("getMapLocation");                        
-    navigator.geolocation.getCurrentPosition
-    (onMapSuccess, onMapError, { enableHighAccuracy: true });
+  navigator.geolocation.getCurrentPosition(success, error);
 }
-
-// Success callback for get geo coordinates
-
-var onMapSuccess = function (position) {
-
-    Latitude = position.coords.latitude;
-    Longitude = position.coords.longitude;
-
-    getMap(Latitude, Longitude);
-
-}
-
-// Get map by using coordinates
-
-function getMap(latitude, longitude) {
-
-    var mapOptions = {
-        center: new google.maps.LatLng(0, 0),
-        zoom: 1,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new google.maps.Map
-    (document.getElementById("map"), mapOptions);
-
-
-    var latLong = new google.maps.LatLng(latitude, longitude);
-
-    var marker = new google.maps.Marker({
-        position: latLong
-    });
-
-    marker.setMap(map);
-    map.setZoom(15);
-    map.setCenter(marker.getPosition());
-}
-
-// Success callback for watching your changing position
-
-var onMapWatchSuccess = function (position) {
-
-    var updatedLatitude = position.coords.latitude;
-    var updatedLongitude = position.coords.longitude;
-
-    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
-
-        Latitude = updatedLatitude;
-        Longitude = updatedLongitude;
-
-        getMap(updatedLatitude, updatedLongitude);
-    }
-}
-
-// Error callback
-
-function onMapError(error) {
-    console.log('code: ' + error.code + '\n' +
-        'message: ' + error.message + '\n');
-        alert("what a fucking error man");
-}
-
-// Watch your changing position
-
-function watchMapPosition() {
-
-    return navigator.geolocation.watchPosition
-    (onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
-}
-
-
-$('#tester').html('foo');
-
-
